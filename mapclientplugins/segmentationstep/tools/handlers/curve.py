d21aef441452f2135898b97468152f633810755e
TimmyNic <tim.nicolas@outlook.com>
2021-09-15 18:12:13 +1200

    PEP8 changes.

--- mapclientplugins/segmentationstep/tools/handlers/curve.py
+++ mapclientplugins/segmentationstep/tools/handlers/curve.py
@@ -1,7 +1,7 @@
-'''
+"""
 MAP Client, a program to generate detailed musculoskeletal models for OpenSim.
     Copyright (C) 2012  University of Auckland
-    
+
 This file is part of MAP Client. (http://launchpad.net/mapclient)
 
     MAP Client is free software: you can redistribute it and/or modify
@@ -16,14 +16,12 @@ This file is part of MAP Client. (http://launchpad.net/mapclient)
 
     You should have received a copy of the GNU General Public License
     along with MAP Client.  If not, see <http://www.gnu.org/licenses/>..
-'''
+"""
 from PySide2 import QtCore
 
 from mapclientplugins.segmentationstep.tools.handlers.abstractselection import AbstractSelection
-from mapclientplugins.segmentationstep.definitions import ViewMode, \
-    DEFAULT_INTERPOLATION_COUNT
-from mapclientplugins.segmentationstep.undoredo import CommandCurveNode, \
-    CommandMovePlane
+from mapclientplugins.segmentationstep.definitions import ViewMode, DEFAULT_INTERPOLATION_COUNT
+from mapclientplugins.segmentationstep.undoredo import CommandCurveNode, CommandMovePlane
 from mapclientplugins.segmentationstep.segmentpoint import ControlPointStatus
 from mapclientplugins.segmentationstep.maths.algorithms import calculateLinePlaneIntersection
 from mapclientplugins.segmentationstep.model.curve import CurveModel
